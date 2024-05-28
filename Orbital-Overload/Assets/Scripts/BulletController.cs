48a52e71e35f3473a9c8f53361483e5cc72b0488
Rishabh Garg <narishabh.garg@gmail.com>
2024-05-28 21:51:49 +0530

    Implemented some shoot system fix

--- Orbital-Overload/Assets/Scripts/BulletController.cs
+++ Orbital-Overload/Assets/Scripts/BulletController.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    private float bulletSpeed;
-    private Vector2 bulletDirection = Vector2.zero;
     private Rigidbody2D rb;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    void FixedUpdate()
-    {
-        ShootBullet();
-    }
-    public void SetBullet(Vector2 direction, float speed)
-    {
-        bulletDirection = direction.normalized;
-        bulletSpeed = speed;
-    }
-    private void ShootBullet()
+    public void ShootBullet(Vector2 bulletDirection, float bulletSpeed)
     {
         rb.velocity = bulletDirection * bulletSpeed * Time.fixedDeltaTime;
     }
