3dc4109327c01897f791240b853c10f3902b0545
Rishabh Garg <narishabh.garg@gmail.com>
2025-01-19 01:40:20 +0530

    Fixed slow-motion logic

--- Orbital-Overload/Assets/Scripts/PowerUp/SubController/SlowMotionPowerUpController.cs
+++ Orbital-Overload/Assets/Scripts/PowerUp/SubController/SlowMotionPowerUpController.cs
@@ -6,6 +6,9 @@ namespace ServiceLocator.PowerUp
 {
     public class SlowMotionPowerUpController : PowerUpController
     {
+        // Private Variables
+        private float defaultFixedDeltaTime;
+
         public SlowMotionPowerUpController(PowerUpData _powerUpData, PowerUpView _powerUpPrefab,
             Transform _powerUpParentPanel, Vector2 _spawnPosition,
             EventService _eventService) :
@@ -13,15 +16,20 @@ namespace ServiceLocator.PowerUp
             base(_powerUpData, _powerUpPrefab,
             _powerUpParentPanel, _spawnPosition,
             _eventService)
-        { }
+        {
+            // Setting Variables
+            defaultFixedDeltaTime = Time.fixedDeltaTime;
+        }
 
         protected override void EnablePowerUp(ActorController _actorController)
         {
-            Time.timeScale = powerUpModel.PowerUpValue; // Slow down time
+            Time.timeScale = Mathf.Lerp(1f, powerUpModel.PowerUpValue, powerUpModel.PowerUpDuration);
+            Time.fixedDeltaTime = defaultFixedDeltaTime * Time.timeScale;
         }
         protected override void DisablePowerUp(ActorController _actorController)
         {
-            Time.timeScale = 1f; // Reset time
+            Time.timeScale = 1f;
+            Time.fixedDeltaTime = defaultFixedDeltaTime * 1f;
         }
     }
 }
